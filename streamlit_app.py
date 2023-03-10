81de75c46bac4114458831f61965b4801b08446c
lceliberti <louieceliberti@icloud.com>
2023-03-10 18:59:11 -0500

    Update streamlit_app.py
    added dataframe

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,3 +8,5 @@ streamlit.text('🥑🍞hardboiled eggs')
 streamlit.text('🥣 oatmeal with fruit')
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+
+streamlit.dataframe(my_fruit_list)
