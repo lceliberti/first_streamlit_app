9e5355e1cc2aeb87d06c984da3a1f5bf9cf1e39f
lceliberti <louieceliberti@icloud.com>
2023-03-10 18:55:20 -0500

    Update streamlit_app.py
    added breakfast menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,7 @@
 import streamlit
 
 streamlit.title('My parents own a diner')
+streamlit.header('Breakfast Menu')
+streamlit.text ('western omelets')
+streamlit.text('hardboiled eggs')
+streamlit.text('oatmeal with fruit')
