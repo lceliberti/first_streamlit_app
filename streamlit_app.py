58e12467c3ebf307dcc28078ea013a4ad1ea2774
lceliberti <louieceliberti@icloud.com>
2023-03-10 18:58:37 -0500

    Update streamlit_app.py
    imported pandas and read file

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,10 @@
 import streamlit
+import pandas
 
 streamlit.title('My parents own a diner')
 streamlit.header('Breakfast Menu')
 streamlit.text ('🐔 western omelets')
 streamlit.text('🥑🍞hardboiled eggs')
 streamlit.text('🥣 oatmeal with fruit')
+
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
